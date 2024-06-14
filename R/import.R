9c25b56a6fbb0f5310fe2e70c6a2f2d417f5bc65
nuclearP <765126696@qq.com>
2024-06-14 17:59:47 +0800

    come on

---
+++ R/import.R
@@ -0,0 +1,7 @@
+#' @importFrom methods new
+#' @importFrom stats IQR
+#' @importFrom stats na.omit
+#' @importFrom stats quantile
+#' @importFrom stats sd
+#' @importFrom data.table fread
+NULL
--- R/object.R
+++ R/object.R
@@ -1,3 +1,7 @@
+#' @include pre_treat.R
+#'
+NULL
+
 #' the sp class
 #'
 #' class designed for one experiment
@@ -14,16 +18,109 @@ sp <- setClass(
     features = "data.frame",
     scaledata = "data.frame",
     dim = "list"
-  )
+  ),
+  prototype = list(features = NULL,scaledata = NULL,dim = NULL)
 )
 
-#' The spatials class
+#' creatsp
+#'
+#' creat sp class
+#'
+#' @param pathway The path where the original data is stored
+#' @param filetype File extension
+#'
+#' @return a sp class
+#' @export
+creatsp <- function(
+  pathway,
+  filetype = "xls"
+){
+  rawdata <- paste0(pathway,"/","rawdata.",filetype)
+  location <- paste0(pathway,"/","location.",filetype)
+  if(!file.exists(rawdata)){
+    stop("Original data not found")
+  }
+  rawdata <- fread(rawdata)
+  row.names(rawdata) <- rawdata$p
+  rawdata <- rawdata[-p]
+
+  if(!file.exists(location)){
+    object <- new(Class = "sp",rawdata = rawdata)
+  }else{
+    location <- fread(location)
+    object <- new(Class = "sp",rawdata = rawdata,features = location)
+  }
+  object
+}
+
+#' @export
+setGeneric("mean_cv",function(object) standardGeneric("mean_cv") )
+
+#' @title mean_cv
+#' @description Calculate mean and cv
+#' @param object  A parameter object of one of the following classe sp or spatials
+#' @return a dataframe containing the mean and standard deviation of each sample
+#' @name mean_cv
+#' @rdname mean_cv
+NULL
+
+#' @rdname mean_cv
+#' @exportMethod mean_cv
+setMethod("mean_cv","sp",function(object){
+  feature <- colSums(!is.na(object@rawdata)) %>% as.data.frame()
+  colnames(feature) <- "num"
+  feature$samples <- row.names(feature)
+  feature$means <- apply(object@rawdata,1,mean,na.rm = T)
+  feature$cvs <- apply(object@rawdata,1,removena_cv)
+  if(is.null(object@features)){
+    object@features <- feature
+  }else{
+    object@features <- merge(object@features,feature,by = "samples")
+  }
+}
+)
+
+
+#' @export
+setGeneric("normlization",function(object,...) standardGeneric("normlization") )
+
+#' @title normlization
+#' @description normalize raw data
+#' @importFrom limma normalizeQuantiles
+#' @param object  A parameter object of one of the following classe sp or spatials
+#' @param meancut  A parameter object of one of the following classe sp or spatials
+#' @param cvcut  A parameter object of one of the following classe sp or spatials
+#' @return A dataframe contains data that has been quality controlled and normalized
+#' @name normlization
+#' @rdname normlization
+NULL
+
+#' @rdname normlization
+#' @exportMethod normlization
+setMethod("normlization","sp",function(object,meancut,cvcut){
+  if(!is.numeric(meancut)){
+    stop("meancut should be a numerical value")
+  }
+  if(!is.numeric(cvcut)){
+    stop("cvcut should be a numerical value")
+  }
+  selected <- subset(object@features,means < meancut,cvs < cvcut)
+  scaledata <- object@rawdata[selected$samples]
+  if(nrow(scaledata) < 1){
+    stop("All data is filtered")
+  }
+  scaledata <- removeRowsAllNa(scaledata) %>% normalizeQuantiles(scaledata)
+  object@scaledata <- scaledata
+}
+)
+
+#' the spatials class
 #'
 #' An S4 object that stores multiple sp objects, but only one sp object is activated at the same time
 #'
 #' @slot experiment list.
 #' @slot active character.
-#' @exportClass spatilas
+#' @exportClass spatials
 spatials <- setClass(
   "spatials",
   slots = c(
@@ -31,3 +128,5 @@ spatials <- setClass(
     active = "character"
   )
 )
+
+
